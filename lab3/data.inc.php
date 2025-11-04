23944dcfc7f3ce7cbf8e629687b6af2438ee14da
Капустина Валерия <lera150595@yandex.ru>
2025-11-04 23:35:52 +0300

    .

---
+++ lab3/data.inc.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+$leftMenu = [
+    ['link' => 'Домой',        'href' => 'index.php'],
+    ['link' => 'О нас',        'href' => 'about.php'],
+    ['link' => 'Контакты',     'href' => 'contact.php'],
+    ['link' => 'Таблица умножения', 'href' => 'table.php'],
+    ['link' => 'Калькулятор',  'href' => 'calc.php']
+];
---
+++ lab3/top.inc.php
@@ -0,0 +1,2 @@
+<img src="/home/f1176557/domains/f1176557.xsph.ru/public_html/lab3/inc/logo.png" width="130" height="80" alt="Наш логотип" class="logo">
+<span class="slogan">приходите к нам учиться</span>
