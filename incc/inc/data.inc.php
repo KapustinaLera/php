72a4512c7750eacef466a161b3956eb49bbac304
KapustinaLera <lera150595@gmail.com>
2025-11-05 13:51:20 +0300

    Add files via upload

---
+++ incc/inc/data.inc.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+$leftMenu = [
+    ['link' => 'Домой',        'href' => 'inc/index.php'],
+    ['link' => 'О нас',        'href' => 'inc/about.php'],
+    ['link' => 'Контакты',     'href' => 'inc/contact.php'],
+    ['link' => 'Таблица умножения', 'href' => 'inc/table.php'],
+    ['link' => 'Калькулятор',  'href' => 'inc/calc.php']
+];
